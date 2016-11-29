b3a09d60e5b56b5c8a595f8a4f2b9e1b7d105d6e
YuTao Wu <yutaowu@YuTaodeMacBook-Pro.local>
2016-11-29 22:43:15 +0800

    calculator keyboard init

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var isTheFirstNumber: Bool = true;
+    
+    @IBAction func appednDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if isTheFirstNumber {
+            display.text = digit
+            isTheFirstNumber = false
+        } else {
+            display.text = display.text! + digit
+        }
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
